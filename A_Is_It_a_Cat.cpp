c7d4e2a81aaaca15184c82a7d59888db9ba9147f
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2024-03-31 00:07:33 +0530

    Day -1

---
+++ A_Is_It_a_Cat.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        string cmp = "meow";
+
+        for(int i=0; i<n; i++){
+            if(s[i] <= 90) s[i]+=32; 
+        }
+
+        string  ans = "";
+
+        for(int i=0; i<n; i++){
+           if(ans.empty() or ans.back() != s[i]){
+                ans.push_back(s[i]);
+           }
+        }
+
+        if(ans == cmp) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+    return 0;
+}
